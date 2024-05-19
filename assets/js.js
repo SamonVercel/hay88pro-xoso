8629e44d8b8b84b14e8d1f1a47a44ffdccc3688e
Hay88Git <myvercel2024@gmail.com>
2024-05-19 08:18:59 +0700

    first commit

---
+++ assets/js.js
@@ -0,0 +1,8 @@
+function ShowNav() {
+    document.getElementById("nav").style.width = "80%";
+    document.getElementById("close").style.display = "block";
+}
+function HideNav() {
+    document.getElementById("nav").style.width = "0%";
+    document.getElementById("close").style.display = "none";
+}
